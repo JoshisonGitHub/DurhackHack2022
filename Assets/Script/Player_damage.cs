41ba35e1bf61dd486bdf3764d0aa5311636b59ce
B1inded <joshua.d.pulham@durham.ac.uk>
2022-11-19 18:09:46 +0000

    Add moving enemy damage

--- Assets/Script/Player_damage.cs
+++ Assets/Script/Player_damage.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_damage : MonoBehaviour
 {
-    private BoxCollider2D box;
+    private BoxCollider box;
     // Start is called before the first frame update
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
+        box = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -23,7 +24,25 @@ public class Player_damage : MonoBehaviour
         {
             //Destroy(gameObject, .5f);
             Debug.Log("Player Death");
+            //Die();
+            
         }
         // check for player collision. If true, playerStatusDeath = true
     }
+
+    IEnumerator Die()
+    {
+        
+
+        
+        yield return new WaitForSeconds(2);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(Application.loadedLevel);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+    }
+    
+
+
 }
