7adb06ae680ccf680edf7c16e22a4c1d22e6fa81
B1inded <joshua.d.pulham@durham.ac.uk>
2022-11-19 15:05:52 +0000

    Add player movement script

--- Assets/player_movement.cs
+++ Assets/player_movement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+    private BoxCollider box;
     // Start is called before the first frame update
     void Start()
     {
-        
+        box = gameObject.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
